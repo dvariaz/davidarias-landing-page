fa55a59f2cce2d447924ef3f6e271ab1a2d142a6
David Arias <davidarias.devs@gmail.com>
2020-09-14 21:11:46 -0500

    Contact section

--- contents/Contact/Contact.jsx
+++ contents/Contact/Contact.jsx
@@ -1,7 +1,22 @@
 import styles from "./Contact.module.scss";
 
 const Contact = () => {
-    return <section className={styles.body}>Contáctame</section>;
+    return (
+        <section className={styles.body}>
+            <div className={styles.form}>
+                <h1 className={styles.title}>
+                    ¿Qué esperas para darle un look fantástico a tus ideas?
+                </h1>
+                <a href="#" className={styles.button}>
+                    Contáctame
+                </a>
+            </div>
+            <div className={styles.background}>
+                <h1 className={styles.backgroundText}>Contact</h1>
+                <h1 className={styles.backgroundText}>Me</h1>
+            </div>
+        </section>
+    );
 };
 
 export default Contact;
