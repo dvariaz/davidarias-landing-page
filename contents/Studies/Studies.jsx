8081f8b015fb52bf2776ed53fa1857e3edfeb76c
David Arias <davidarias.devs@gmail.com>
2020-09-16 20:22:14 -0500

    Studies navigator finished

--- contents/Studies/Studies.jsx
+++ contents/Studies/Studies.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from "react";
+
 import styles from "./Studies.module.scss";
 
 import Card from "@components/Card";
@@ -5,6 +7,48 @@ import Card from "@components/Card";
 //TODO: Hacer que se muestren los cursos de la plataforma activa
 
 const Studies = ({ studies }) => {
+    const listRef = useRef();
+    const [selectedInstitute, setSelectedInstitute] = useState(null);
+
+    const renderCourses = () => {
+        let courses = [];
+
+        if (!selectedInstitute) {
+            studies.forEach((study) => {
+                study.courses.forEach((course) => courses.push({ ...course, icon: study.logo }));
+            });
+        } else {
+            let [study] = studies.filter((study) => {
+                return study.id === selectedInstitute;
+            });
+
+            courses = study.courses.map((course) => ({ ...course, icon: study.logo }));
+        }
+
+        return courses.map((course) => (
+            <li key={course.id}>
+                <Card
+                    type="course"
+                    subtitle={course.name}
+                    icon={course.icon}
+                    date={course.date}
+                    duration={course.duration}
+                />
+            </li>
+        ));
+    };
+
+    const selectInstitute = (e) => {
+        const id = e.target.value;
+        if (selectedInstitute == id) {
+            setSelectedInstitute(null);
+        } else {
+            setSelectedInstitute(id);
+        }
+
+        listRef.current.scrollTop = 0;
+    };
+
     return (
         <section className={styles.body}>
             <h1 className={styles.title}>Estudios</h1>
@@ -20,27 +64,19 @@ const Studies = ({ studies }) => {
                             <input
                                 type="radio"
                                 name="platformSelected"
+                                value={study.id}
+                                checked={selectedInstitute == study.id}
+                                onClick={selectInstitute}
                                 className={styles.platform}
                                 style={{ backgroundImage: `url('${study.logo}')` }}
+                                readOnly
                             />
                         </label>
                     ))}
                 </form>
             </div>
-            <ul className={styles.courses}>
-                {studies.map((study) =>
-                    study.courses.map((course) => (
-                        <li index={course.id}>
-                            <Card
-                                type="course"
-                                subtitle={course.name}
-                                icon={study.logo}
-                                date={course.date}
-                                duration={course.duration}
-                            />
-                        </li>
-                    ))
-                )}
+            <ul ref={listRef} className={styles.courses}>
+                {renderCourses()}
             </ul>
         </section>
     );
