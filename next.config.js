225590082aad3cfa14744081ea9a5b944219aaa3
David Arias <davidarias.devs@gmail.com>
2020-09-19 20:42:43 -0500

    Webpack aliases config legacy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 
 module.exports = {
-    webpack: function (config) {
-        config.resolve.alias["@"] = path.join(__dirname, ".");
+    webpack(config) {
+        config.resolve.alias["@components"] = path.join(__dirname, "components");
+        config.resolve.alias["@hooks"] = path.join(__dirname, "hooks");
+        config.resolve.alias["@contents"] = path.join(__dirname, "contents");
+        config.resolve.alias["@settings"] = path.join(__dirname, "styles");
         return config;
     },
 };
