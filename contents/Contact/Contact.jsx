65759e3df669426a70821df0f5bdbe276580039d
David Arias <davidarias.devs@gmail.com>
2020-09-24 19:08:25 -0500

    Social buttons in contact

--- contents/Contact/Contact.jsx
+++ contents/Contact/Contact.jsx
@@ -1,8 +1,10 @@
 import styles from "./Contact.module.scss";
 
-const Contact = () => {
+//TODO: Insertar iconos de redes sociales y algunas marcas de copyright si es posible, año
+
+const Contact = ({ id }) => {
     return (
-        <section className={styles.body}>
+        <section id={id} className={styles.body}>
             <div className={styles.form}>
                 <h1 className={styles.title}>
                     ¿Qué esperas para darle un look fantástico a tus ideas?
@@ -11,6 +13,33 @@ const Contact = () => {
                     Contáctame
                 </a>
             </div>
+            <div className={styles.social}>
+                Sígueme en redes sociales
+                <div className={styles.socialNetworks}>
+                    <a href="https://www.linkedin.com/in/dvariaz/" target="_blank">
+                        <img
+                            src={"/assets/icons/linkedin_icon.svg"}
+                            className={styles.icon}
+                            alt="Github"
+                        />
+                    </a>
+                    <a href="https://www.behance.net/dvariaz" target="_blank">
+                        <img
+                            src={"/assets/icons/behance_icon.svg"}
+                            className={styles.icon}
+                            alt="Behance"
+                        />
+                    </a>
+                    <a href="https://github.com/dvariaz" target="_blank">
+                        <img
+                            src={"/assets/icons/github_icon.svg"}
+                            className={styles.icon}
+                            alt="Github"
+                        />
+                    </a>
+                </div>
+            </div>
+            <footer className={styles.footer}>2020</footer>
             <div className={styles.background}>
                 <h1 className={styles.backgroundText}>Contact</h1>
                 <h1 className={styles.backgroundText}>Me</h1>
