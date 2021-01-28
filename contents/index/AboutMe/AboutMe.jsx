b6ff61cd4c510589a18df815182cbf691370b064
David Arias <davidarias.devs@gmail.com>
2021-01-27 22:51:26 -0500

    CV and position update

--- contents/index/AboutMe/AboutMe.jsx
+++ contents/index/AboutMe/AboutMe.jsx
@@ -6,45 +6,45 @@ import Divider from "../../../components/Divider";
 import Button from "../../../components/Button";
 
 const AboutMe = ({ t }) => {
-    return (
-        <section className={styles.body}>
-            <Divider top />
-            <div className={styles.content}>
-                <h3>{t("greeting")}</h3>
-                <img src="/assets/logos/davidarias_logo.svg" className={styles.logo} />
-                <div className={styles.description}>
-                    <p>{t("description")}</p>
-                    <p>
-                        {t("current-job")}
-                        <a href="https://www.woldev.co" target="_blank">
-                            Woldev.co
-                        </a>
-                    </p>
-                    <p>React Lover ⚛️</p>
-                </div>
-                <Button type="solid" href={t("cv-link")} target="_blank">
-                    {t("cv-link-button")}
-                </Button>
-            </div>
-            <div className={styles.background}>
-                <motion.h1
-                    animate={{ opacity: [0, 1, 0], x: "-30%" }}
-                    transition={{ type: "tween", loop: Infinity, duration: 10 }}
-                    className={styles.backgroundText}
-                >
-                    David
-                </motion.h1>
-                <motion.h1
-                    animate={{ opacity: [0, 1, 0], x: "30%" }}
-                    transition={{ type: "tween", loop: Infinity, duration: 10 }}
-                    className={styles.backgroundText}
-                >
-                    Arias
-                </motion.h1>
-            </div>
-            <Divider />
-        </section>
-    );
+  return (
+    <section className={styles.body}>
+      <Divider top />
+      <div className={styles.content}>
+        <h3>{t("greeting")}</h3>
+        <img src="/assets/logos/davidarias_logo.svg" className={styles.logo} />
+        <div className={styles.description}>
+          <p>{t("description")}</p>
+          <p>
+            {t("current-job")}
+            <a href="https://www.nuvu.cc" target="_blank">
+              Nuvu
+            </a>
+          </p>
+          <p>React Lover ⚛️</p>
+        </div>
+        <Button type="solid" href={t("cv-link")} target="_blank">
+          {t("cv-link-button")}
+        </Button>
+      </div>
+      <div className={styles.background}>
+        <motion.h1
+          animate={{ opacity: [0, 1, 0], x: "-30%" }}
+          transition={{ type: "tween", loop: Infinity, duration: 10 }}
+          className={styles.backgroundText}
+        >
+          David
+        </motion.h1>
+        <motion.h1
+          animate={{ opacity: [0, 1, 0], x: "30%" }}
+          transition={{ type: "tween", loop: Infinity, duration: 10 }}
+          className={styles.backgroundText}
+        >
+          Arias
+        </motion.h1>
+      </div>
+      <Divider />
+    </section>
+  );
 };
 
 export default AboutMe;
