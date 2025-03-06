c428dc54d8f273181e4d846ff43dba8a43a37379
David Arias <davidarias.devs@gmail.com>
2025-03-05 21:36:59 -0500

    feat(Constants): moved to config folder

---
+++ config/constants/skills.js
@@ -0,0 +1,87 @@
+export const FRONTEND_TECHNOLOGIES = [
+  {
+    prefix: "/assets/icons/vue_icon.svg",
+    name: "Vue",
+  },
+  {
+    prefix: "/assets/icons/nuxt_icon.svg",
+    name: "Nuxt",
+  },
+  {
+    prefix: "/assets/icons/react_icon.svg",
+    name: "React",
+  },
+  {
+    prefix: "/assets/icons/next_icon.svg",
+    name: "Next",
+  },
+  {
+    prefix: "/assets/icons/html_icon.svg",
+    name: "HTML",
+  },
+  {
+    prefix: "/assets/icons/css_icon.svg",
+    name: "CSS",
+  },
+  {
+    prefix: "/assets/icons/sass_icon.svg",
+    name: "Sass",
+  },
+  {
+    prefix: "/assets/icons/tailwind_css_icon.svg",
+    name: "Tailwind",
+  },
+  {
+    prefix: "/assets/icons/javascript_icon.svg",
+    name: "Javascript",
+  },
+  {
+    prefix: "/assets/icons/typescript_icon.svg",
+    name: "Typescript",
+  },
+]
+
+export const BACKEND_TECHNOLOGIES = [
+  {
+    prefix: "/assets/icons/nodejs_icon.svg",
+    name: "Node.js",
+  },
+]
+
+export const DESIGN_SKILLS = [
+  {
+    prefix: "/assets/icons/photoshop_icon.svg",
+    name: "Photoshop",
+  },
+  {
+    prefix: "/assets/icons/aftereffects_icon.svg",
+    name: "After Effects",
+  },
+  {
+    prefix: "/assets/icons/illustrator_icon.svg",
+    name: "Illustrator",
+  },
+  {
+    prefix: "/assets/icons/adobexd_icon.svg",
+    name: "XD",
+  },
+  {
+    prefix: "/assets/icons/figma_icon.svg",
+    name: "Figma",
+  },
+]
+
+export const TESTING_SKILLS = [
+  {
+    prefix: "/assets/icons/jest_icon.svg",
+    name: "Jest",
+  },
+  {
+    prefix: "/assets/icons/testing_library_icon.svg",
+    name: "Testing Library",
+  },
+  {
+    prefix: "/assets/icons/selenium_icon.svg",
+    name: "Selenium",
+  },
+]
