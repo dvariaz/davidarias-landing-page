01424070d56543019abd3e7e4e9428be811462d0
David Arias <davidarias.devs@gmail.com>
2025-03-05 23:12:44 -0500

    feat(Env): vercel url changed to app base path (#3)
    * feat(Env): vercel url changed to app base path
    
    * feat(Env): env config on next config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    APP_BASE_URL: process.env.APP_BASE_URL || `https://${process.env.VERCEL_URL}`,
+  }
+};
  
 export default withNextIntl(nextConfig);
